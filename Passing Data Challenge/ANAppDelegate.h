c49db44ce855c22fa256efe6276b04b2b2c1b0d6
IOStudent <anna.veronica@gmail.com>
2014-03-25 15:54:23 -0400

    Initial Commit

---
+++ Passing Data Challenge/ANAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ANAppDelegate.h
+//  Passing Data Challenge
+//
+//  Created by Anca Negrean on 3/25/14.
+//  Copyright (c) 2014 Radu Negrean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ANAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Passing Data Challenge/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Passing Data Challenge
+//
+//  Created by Anca Negrean on 3/25/14.
+//  Copyright (c) 2014 Radu Negrean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ANAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ANAppDelegate class]));
+    }
+}
